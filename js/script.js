4b206fdf4e9cae986ec0ff9d49c99721acef99ad
José de los santos <delossantosramosjose@gmail.com>
2023-09-12 01:17:13 +0200

    Light and plane added

--- js/script.js
+++ js/script.js
@@ -4,18 +4,46 @@ window.addEventListener("load", onload)
 
 function onload(){
     scene = new THREE.Scene()
-    renderer = new THREE.WebGLRenderer()
+    // scene.background = new THREE.Color(0x333333)
+    renderer = new THREE.WebGLRenderer({alpha: true})
+    renderer.shadowMap.enabled = true
+
     document.body.appendChild(renderer.domElement)
     
     createCube()
+    // createGrid()
+    createLight()
+    createPlane()
     animate()
 }
 
 function createCube(){
-    const geometry = new THREE.BoxGeometry()
-    const material = new THREE.MeshBasicMaterial({color: 0x0000ff}) // color blue
+    const geometry = new THREE.BoxGeometry(1,1,1)
+    const material = new THREE.MeshStandardMaterial({color: 0x0000ff}) // color blue
     cube = new THREE.Mesh(geometry, material)
+    cube.position.set(0,0,1)
+    cube.castShadow = true
     scene.add(cube)
+}
+function createLight(){
+    const light = new THREE.DirectionalLight(0xffffff, 1, 10)
+    light.position.set(1,1,1)
+    light.castShadow = true
+    scene.add(light)
+}
+
+function createPlane(){
+    const geometry = new THREE.PlaneGeometry(20,20,32,32)
+    const material = new THREE.MeshStandardMaterial({color: 0x55ff00}) // color blue
+    const plane = new THREE.Mesh(geometry, material)
+    plane.receiveShadow = true
+    plane.position.set(0,0,0)
+    scene.add(plane)
+}
+
+function createGrid(){
+    const grid = new THREE.GridHelper(100, 100)
+    scene.add(grid)
 }
 
 function animate(){
@@ -36,4 +64,6 @@ function resize(){
     )
     renderer.setSize(window.innerWidth, window.innerHeight)
     camera.position.z = 5
+    camera.position.y = -10
+    camera.rotation.x = 1
 }
