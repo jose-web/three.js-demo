5adb734ab4fd39c2faf96f1aa85fdb0bd9666d79
José de los santos <delossantosramosjose@gmail.com>
2023-09-11 23:55:26 +0200

    Add a cube

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
+
+
+window.addEventListener("load", onload)
+
+function onload(){
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
         75, // Camera frustum vertical field of view
         window.innerWidth / window.innerHeight, // Camera frustum aspect ratio
         0.1, // Camera frustum near plane
         1000 // Camera frustum far plane
     )
+    camera.position.z = 5 // turn the camera back
+    
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+    
+    createCube(scene)
+    
+    renderer.render(scene, camera)
+}
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-renderer.render(scene, camera)
+function createCube(scene){
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshBasicMaterial({color: 0x0000ff}) // color blue
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+}
