d8176d1839bc780b1d98c93b8e1685878fa31e6e
José de los santos <delossantosramosjose@gmail.com>
2023-09-12 00:08:56 +0200

    Animation to the cube added, and resize solved

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
-
+let scene, camera, renderer, cube
 
 window.addEventListener("load", onload)
 
 function onload(){
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(
+    scene = new THREE.Scene()
+    renderer = new THREE.WebGLRenderer()
+    document.body.appendChild(renderer.domElement)
+    
+    createCube()
+    animate()
+}
+
+function createCube(){
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshBasicMaterial({color: 0x0000ff}) // color blue
+    cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+}
+
+function animate(){
+    requestAnimationFrame(animate)
+    resize()
+
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+}
+
+function resize(){
+    camera = new THREE.PerspectiveCamera(
         75, // Camera frustum vertical field of view
         window.innerWidth / window.innerHeight, // Camera frustum aspect ratio
         0.1, // Camera frustum near plane
         1000 // Camera frustum far plane
     )
-    camera.position.z = 5 // turn the camera back
-    
-    const renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-    
-    createCube(scene)
-    
-    renderer.render(scene, camera)
-}
-
-function createCube(scene){
-    const geometry = new THREE.BoxGeometry()
-    const material = new THREE.MeshBasicMaterial({color: 0x0000ff}) // color blue
-    const cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
+    camera.position.z = 5
 }
